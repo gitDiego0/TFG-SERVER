87dcaa8074b56b438e6a6c83ccbee08de4d767b0
Diego Garcia <diegogarcianavas.4b@gmail.com>
2021-06-14 13:33:43 +0200

    contact email styled

--- src/routes/contactFormEmail.js
+++ src/routes/contactFormEmail.js
@@ -26,7 +26,25 @@ router.post("/contact", async (req, res) => {
       to: `${emailInput},contact.hostalapp@gmail.com`,
       subject: `${nombre} ${apellidos}: ${subject}`,
       text: `${Mensaje}`,
-      html: `${Mensaje}<br><br>${emailInput}`,
+      html: `
+                  <div>
+                    <h1>Gracias por su mensaje ${valores.nombre}</h1>
+                    <h3>Lo revisaremos lo mas pronto posible y nos pondremos en contacto en caso necesario</h3>
+                  </div>
+                  <div>
+                    <p>Datos del mensaje:</p>
+                    <div>
+                      <p>Nombre: ${nombre}</p>
+                      <p>Apellidos: ${apellidos}</p>                        
+                      <p>Email: ${emailInput}</p>
+                      <p>Asunto: ${subject}</p>
+                        <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+                        <br>
+                        <p>${Mensaje}</p>
+                        
+                    </div>
+                  </div>
+                  `,
     },
     (err, info) => {
       if (err) console.log(err);
